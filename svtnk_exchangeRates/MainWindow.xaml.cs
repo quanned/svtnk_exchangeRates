30568af84304fc25f5790463d6480b7de91c6983
quanned <avdeyartem1@gmail.com>
2020-06-23 15:56:14 +0300

    work_23062020_1600

--- svtnk_exchangeRates/MainWindow.xaml.cs
+++ svtnk_exchangeRates/MainWindow.xaml.cs
@@ -32,6 +32,18 @@ namespace svtnk_exchangeRates
         {
             InitializeComponent();
 
+            if (CheckInternet().Equals(2))
+            {
+                MessageBox.Show("Проверьте доступ к Интернету", "Внимание", MessageBoxButton.OKCancel);
+            }
+            //else
+            //{
+            //    MessageBox.Show("все окэй", "Внимание", MessageBoxButton.OKCancel);
+            //}
+            
+            DateTime thisDay = DateTime.Today;
+            DP.Text = thisDay.ToString();
+
             Data data = JsonConvert.DeserializeObject<Data>(json);
             foreach (Item item in data.Rate.Items)
             {
@@ -43,20 +55,64 @@ namespace svtnk_exchangeRates
             {
                 MessageBox.Show("Введите значения в файл" + pathToCurIDFile);
             }
-
-            string[] curIDStrList = GetCurIDList(pathToCurIDFile);        
-            for (int i = 0; i <= curIDStrCount; i++)
+            string[] curIDStrArray = new string[curIDStrCount];
+            for (int i = 0; i < curIDStrCount; i++)
             {
-                TempLB.Items.Add(curIDStrList[i]);
-            }        
-
-            if (curIDStrCount == 0)
-            {
-                MessageBox.Show("Введите значения в файл" + pathToCurIDFile);
+                curIDStrArray[i] = GetCurIDList(pathToCurIDFile)[i];
+                TempLB.Items.Add(curIDStrArray[i]);
             }
+
+            string testUrl = "https://www.nbrb.by/api/exrates/rates/USD?ondate=2020-03-13&parammode=2";
+            TB.Text = GetJSON(testUrl);
+
         }
 
-        const string pathToCurIDFile = @"J:\NSIG\rates\rates.config";
+        static string GetJSON(string uri)
+        {
+
+            return 0.ToString();
+        }
+
+        public enum ConnectionStatus
+        {
+            NotConnected,
+            LimitedAccess,
+            Connected
+        }
+
+        public static ConnectionStatus CheckInternet()
+        {
+            // Проверить загрузку документа ncsi.txt
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create("http://www.msftncsi.com/ncsi.txt");
+            try
+            {
+                HttpWebResponse responce = (HttpWebResponse)request.GetResponse();
+
+                if (responce.StatusCode != HttpStatusCode.OK)
+                {
+                    return ConnectionStatus.LimitedAccess;
+                }
+                using (StreamReader sr = new StreamReader(responce.GetResponseStream()))
+                {
+                    if (sr.ReadToEnd().Equals("Microsoft NCSI"))
+                    {
+                        return ConnectionStatus.Connected;
+                    }
+                    else
+                    {
+                        return ConnectionStatus.LimitedAccess;
+                    }
+                }
+            }
+            catch
+            {
+                return ConnectionStatus.NotConnected;
+            }
+
+        }
+
+
+        const string pathToCurIDFile = @"D:\work\svtnk_exchangeRates\docs\rates.list";
 
         public static int GetCurIDCount(string path)
         {
@@ -88,7 +144,8 @@ namespace svtnk_exchangeRates
             string[] codeList = new string[arrayLength];
 
             codeList = File.ReadAllLines(path);
-    
+            return codeList;
+
             //using (StreamReader sr = new StreamReader(path))
             //{
             //    var list = new List<string>();
@@ -107,13 +164,12 @@ namespace svtnk_exchangeRates
             //        codeList[i] = arrTheoria[i];
             //    }
             //}
-
-            return codeList;
         }
 
         //https://www.nbrb.by/api/exrates/rates/840?parammode=1
         //https://www.nbrb.by/api/exrates/rates/840?ondate=2020-03-13&parammode=1
         //https://www.nbrb.by/api/exrates/rates/298?ondate=2016-7-5
+        //https://www.nbrb.by/api/exrates/rates/USD?ondate=2016-7-5         выгоднее юзать это, применив знание госта в файле
 
 
         class Data
@@ -137,7 +193,7 @@ namespace svtnk_exchangeRates
 
             public override string ToString()
             {
-                return string.Format("{0}: {1}, {2}", Cur_Abbreviation, Date, Cur_OfficialRate);
+                return string.Format("{0} ({1}, {2}): {3}, Date: {4}", Cur_Name, Cur_Abbreviation, Cur_ID, Cur_OfficialRate, Date);
             }
         }
 
@@ -152,199 +208,7 @@ namespace svtnk_exchangeRates
                 ""Cur_Scale"": 1,
                 ""Cur_Name"": ""Австралийский доллар"",
                 ""Cur_OfficialRate"": 1.5039
-              },
-              {
-                ""Cur_ID"": 191,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""BGN"",
-                ""Cur_Scale"": 1,
-                ""Cur_Name"": ""Болгарский лев"",
-                ""Cur_OfficialRate"": 1.1420
-              },
-              {
-                ""Cur_ID"": 290,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""UAH"",
-                ""Cur_Scale"": 100,
-                ""Cur_Name"": ""Гривен"",
-                ""Cur_OfficialRate"": 8.0627
-              },
-              {
-                ""Cur_ID"": 291,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""DKK"",
-                ""Cur_Scale"": 10,
-                ""Cur_Name"": ""Датских крон"",
-                ""Cur_OfficialRate"": 3.0022
-              },
-              {
-                ""Cur_ID"": 145,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""USD"",
-                ""Cur_Scale"": 1,
-                ""Cur_Name"": ""Доллар США"",
-                ""Cur_OfficialRate"": 2.0048
-              },
-              {
-                ""Cur_ID"": 292,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""EUR"",
-                ""Cur_Scale"": 1,
-                ""Cur_Name"": ""Евро"",
-                ""Cur_OfficialRate"": 2.2354
-              },
-              {
-                ""Cur_ID"": 293,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""PLN"",
-                ""Cur_Scale"": 10,
-                ""Cur_Name"": ""Злотых"",
-                ""Cur_OfficialRate"": 5.0219
-              },
-              {
-                ""Cur_ID"": 303,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""IRR"",
-                ""Cur_Scale"": 100000,
-                ""Cur_Name"": ""Иранских риалов"",
-                ""Cur_OfficialRate"": 6.5540
-              },
-              {
-                ""Cur_ID"": 294,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""ISK"",
-                ""Cur_Scale"": 100,
-                ""Cur_Name"": ""Исландских крон"",
-                ""Cur_OfficialRate"": 1.6351
-              },
-              {
-                ""Cur_ID"": 295,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""JPY"",
-                ""Cur_Scale"": 100,
-                ""Cur_Name"": ""Йен"",
-                ""Cur_OfficialRate"": 1.9704
-              },
-              {
-                ""Cur_ID"": 23,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""CAD"",
-                ""Cur_Scale"": 1,
-                ""Cur_Name"": ""Канадский доллар"",
-                ""Cur_OfficialRate"": 1.5544
-              },
-              {
-                ""Cur_ID"": 304,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""CNY"",
-                ""Cur_Scale"": 10,
-                ""Cur_Name"": ""Китайских юаней"",
-                ""Cur_OfficialRate"": 3.0051
-              },
-              {
-                ""Cur_ID"": 72,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""KWD"",
-                ""Cur_Scale"": 1,
-                ""Cur_Name"": ""Кувейтский динар"",
-                ""Cur_OfficialRate"": 6.6482
-              },
-              {
-                ""Cur_ID"": 296,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""MDL"",
-                ""Cur_Scale"": 10,
-                ""Cur_Name"": ""Молдавских леев"",
-                ""Cur_OfficialRate"": 1.0119
-              },
-              {
-                ""Cur_ID"": 286,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""NZD"",
-                ""Cur_Scale"": 1,
-                ""Cur_Name"": ""Новозеландский доллар"",
-                ""Cur_OfficialRate"": 1.4445
-              },
-              {
-                ""Cur_ID"": 297,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""NOK"",
-                ""Cur_Scale"": 10,
-                ""Cur_Name"": ""Норвежских крон"",
-                ""Cur_OfficialRate"": 2.4049
-              },
-              {
-                ""Cur_ID"": 298,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""RUB"",
-                ""Cur_Scale"": 100,
-                ""Cur_Name"": ""Российских рублей"",
-                ""Cur_OfficialRate"": 3.1208
-              },
-              {
-                ""Cur_ID"": 299,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""XDR"",
-                ""Cur_Scale"": 1,
-                ""Cur_Name"": ""СДР(Специальные права заимствования)"",
-                ""Cur_OfficialRate"": 2.7986
-              },
-              {
-                ""Cur_ID"": 119,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""SGD"",
-                ""Cur_Scale"": 1,
-                ""Cur_Name"": ""Сингапурcкий доллар"",
-                ""Cur_OfficialRate"": 1.4855
-              },
-              {
-                ""Cur_ID"": 300,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""KGS"",
-                ""Cur_Scale"": 100,
-                ""Cur_Name"": ""Сомов"",
-                ""Cur_OfficialRate"": 2.9738
-              },
-              {
-                ""Cur_ID"": 301,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""KZT"",
-                ""Cur_Scale"": 1000,
-                ""Cur_Name"": ""Тенге"",
-                ""Cur_OfficialRate"": 5.9490
-              },
-              {
-                ""Cur_ID"": 302,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""TRY"",
-                ""Cur_Scale"": 10,
-                ""Cur_Name"": ""Турецких лир"",
-                ""Cur_OfficialRate"": 6.8747
-              },
-              {
-                ""Cur_ID"": 143,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""GBP"",
-                ""Cur_Scale"": 1,
-                ""Cur_Name"": ""Фунт стерлингов"",
-                ""Cur_OfficialRate"": 2.6340
-              },
-              {
-                ""Cur_ID"": 305,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""CZK"",
-                ""Cur_Scale"": 100,
-                ""Cur_Name"": ""Чешских крон"",
-                ""Cur_OfficialRate"": 8.2533
-              },
-              {
-                ""Cur_ID"": 306,
-                ""Date"": ""2016-07-06T00:00:00"",
-                ""Cur_Abbreviation"": ""SEK"",
-                ""Cur_Scale"": 10,
-                ""Cur_Name"": ""Шведских крон"",
-                ""Cur_OfficialRate"": 2.3705
-              },
+              },            
               {
                 ""Cur_ID"": 130,
                 ""Date"": ""2016-07-06T00:00:00"",
@@ -356,5 +220,6 @@ namespace svtnk_exchangeRates
             ]
           }
         }";
+
     };
 }
